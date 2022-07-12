3e5b597cc0582090c602acd8ea7b20c3776fd3fd
steve <sriddiford@bglcorp.com.au>
2022-07-13 07:22:44 +1000

    add song to playlist

--- infra/lambda/add-song-to-playlist/addSongToPlaylist.ts
+++ infra/lambda/add-song-to-playlist/addSongToPlaylist.ts
@@ -3,6 +3,7 @@ import {APIGatewayEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
 
 export const handler = async function (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
 
+    console.log(JSON.stringify(event, null, '  '));
 
     return {
         statusCode: 200,
