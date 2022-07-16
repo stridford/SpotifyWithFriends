6fa4a5fe28b126f1d67a27a4a417ff4987407461
steve <sriddiford@bglcorp.com.au>
2022-07-16 13:33:50 +1000

    loading indicator centered

--- client/src/TrackResults.tsx
+++ client/src/TrackResults.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import {SpotifySearchTrackResult} from "./SpotifySearchTrackResult";
-import Typography from "@mui/material/Typography";
-import {Stack} from "@mui/material";
+import {CircularProgress, Stack} from "@mui/material";
+import Box from "@mui/material/Box";
 
 export function TrackResults(props: { searchResultDTOS: SearchResultDTO[], isSearching: boolean, onTrackClick: (track: SearchResultDTO) => void }) {
     const searchResultDTOS = props.searchResultDTOS;
     const isSearching = props.isSearching;
-    if (isSearching) {
-        return (<Typography>Loading...</Typography>)
-    }
     return (<Stack spacing={1} sx={{ml: 3, mr: 3}}>
+        {isSearching ? <Box
+            display="flex"
+            justifyContent="center">
+            <CircularProgress />
+        </Box>   : null}
         {searchResultDTOS.map(track => <SpotifySearchTrackResult key={track.trackId}
                                                                  onTrackClick={props.onTrackClick}
                                                                  track={track}/>)}
